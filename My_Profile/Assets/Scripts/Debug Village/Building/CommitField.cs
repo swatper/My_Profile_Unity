07e5c79e1a49c7380250e863e8d9e2a9ed72b166
swatper <sh010510@naver.com>
2026-04-06 19:59:21 +0900

    스프라이트 수정
    잡티 제거

--- My_Profile/Assets/Scripts/Debug Village/Building/CommitField.cs
+++ My_Profile/Assets/Scripts/Debug Village/Building/CommitField.cs
@@ -6,8 +6,10 @@ public class CommitField : MonoBehaviour
 {
     [SerializeField] SpriteRenderer sprite;
     [SerializeField] Sprite[] filedSprite;
+    [SerializeField] string date;
     public bool isCommited = false;
-    public void InitFiled(bool commit) {
+    public void InitFiled(bool commit, string commitDate) {
+        date = commitDate;
         isCommited = commit;
         if (commit)
             sprite.sprite = filedSprite[1];
--- My_Profile/Assets/Scripts/Debug Village/ETC/GithubChecker.cs
+++ My_Profile/Assets/Scripts/Debug Village/ETC/GithubChecker.cs
@@ -14,13 +14,14 @@ public class GithubChecker : MonoBehaviour
 
     IEnumerator GetGithubEvents()
     {
-        string url = $"https://api.github.com/users/{username}/events/public";
+        string url = $"https://api.github.com/users/{username}/events/public?per_page=100";
         using (UnityWebRequest webRequest = UnityWebRequest.Get(url)){
             yield return webRequest.SendWebRequest();
 
             if (webRequest.result == UnityWebRequest.Result.Success)
             {
                 string json = webRequest.downloadHandler.text;
+                Debug.Log("받은 Json 데이터: \n" + json);
 
                 for (int i = 0; i < 14; i++){
                     //i일 전의 날짜 문자열 생성 (UTC 기준)
@@ -29,11 +30,11 @@ public class GithubChecker : MonoBehaviour
                     bool isCommited = json.Contains(targetDate) && json.Contains("PushEvent");
 
                     if (isCommited){
-                        commitFields[i].InitFiled(true);
+                        commitFields[i].InitFiled(true, targetDate);
                         Debug.Log($"{targetDate} : 커밋 확인됨!");
                     }
                     else{
-                        commitFields[i].InitFiled(false);
+                        commitFields[i].InitFiled(false, targetDate);
                         Debug.Log($"{targetDate} : 기록 없음.");
                     }
                 }
